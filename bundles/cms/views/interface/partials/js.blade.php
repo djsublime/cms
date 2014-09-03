49097be1078d41dc317ba8ad40e8d71e55d085a3
Dragan Jovanovic <dragan.doe@gmail.com>
2014-09-04 00:55:02 +0200

    fixed issue with laravel profiler

--- bundles/cms/views/interface/partials/js.blade.php
+++ bundles/cms/views/interface/partials/js.blade.php
@@ -6,3 +6,4 @@ var ANALYTICS_ID = '{{Config::get('cms::settings.analytics.id')}}';
 var disqus_shortname = '{{strtolower(Config::get('cms::theme.project_name'))}}';
 var ELE_CLASS = '{{Config::get('cms::theme.ele_class')}}';
 var SITE_CSS = '{{Config::get('cms::theme.asset.site_css.path')}}';
+<?php exit(); ?>
